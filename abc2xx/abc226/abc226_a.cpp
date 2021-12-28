86bbb5712234cc1323e94cc082aff76903ae199a
takashigoto1207 <takashigoto1207@gmail.com>
2021-12-29 00:28:16 +0900

    20211229_01

---
+++ abc2xx/abc226/abc226_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double X;
+  cin >> X;
+
+  cout << (int)(X + 0.5) << endl;
+  return 0;
+}
