9e2fe44f32130be82f1936179ef6fa839b42ddf7
takashigoto1207 <takashigoto1207@gmail.com>
2022-01-06 12:39:13 +0900

    20220106_01

---
+++ abc2xx/abc223/abc223_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int X;
+  cin >> X;
+
+  if (X > 0 && X % 100 == 0)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
