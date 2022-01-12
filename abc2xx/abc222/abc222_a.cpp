0532bf2e94f20b4946fb4ac01006c82e15267bc5
takashigoto1207 <takashigoto1207@gmail.com>
2022-01-12 12:22:53 +0900

    20220112_01

---
+++ abc2xx/abc222/abc222_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string N;
+  cin >> N;
+
+  cout << setfill('0') << right << setw(4) << N << endl;
+  return 0;
+}
