98a2a8d50f10a36f75d4fa6e143c39ff73c725b9
takashigoto1207 <takashigoto1207@gmail.com>
2022-02-01 22:07:20 +0900

    20220201_01

---
+++ abc2xx/abc237/abc237_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll N;
+  cin >> N;
+
+  if (-1ll << 31 <= N && N < 1ll << 31)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
