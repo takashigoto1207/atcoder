4db7919719f5353bc2a1f464471a64ad5c022a4b
takashigoto1207 <takashigoto1207@gmail.com>
2022-02-04 12:09:07 +0900

    20220204_01

---
+++ abc2xx/abc217/abc217_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S, T;
+  cin >> S >> T;
+
+  if (S < T)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
