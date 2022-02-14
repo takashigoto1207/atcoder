c0cd51a43f4970882aa07210c28303c7e9f4aa7a
takashigoto1207 <takashigoto1207@gmail.com>
2022-02-14 20:36:11 +0900

    20220214_01

---
+++ abc2xx/abc215/abc215_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S == "Hello,World!")
+    cout << "AC" << endl;
+  else
+    cout << "WA" << endl;
+  return 0;
+}
