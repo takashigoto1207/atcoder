9fa548ee229d9188e09bae9c7da4435110a68469
takashigoto1207 <takashigoto1207@gmail.com>
2022-03-07 00:12:58 +0900

    20220307_01

---
+++ abc2xx/abc242/abc242_b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+  sort(S.begin(), S.end());
+  cout << S << endl;
+  return 0;
+}
