e94a333b7e63e2c6ad1625152858d58c5e58de06
takashigoto1207 <takashigoto1207@gmail.com>
2022-04-02 11:59:46 +0900

    20220402_01

---
+++ abc2xx/abc205/abc205_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double A, B;
+  cin >> A >> B;
+
+  cout << fixed << setprecision(10) << A * B / 100 << endl;
+  return 0;
+}
