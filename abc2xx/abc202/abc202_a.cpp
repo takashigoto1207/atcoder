1ec7fe94d8658238e59ae1ee4fe27fbb40fe8d08
takashigoto1207 <takashigoto1207@gmail.com>
2022-04-15 00:10:07 +0900

    20220415_01

---
+++ abc2xx/abc202/abc202_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  cout << 21 - a - b - c << endl;
+  return 0;
+}
