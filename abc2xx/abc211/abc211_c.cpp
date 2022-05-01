e1b861b58784c3ebbc3e9440113ea0165e204e75
takashigoto1207 <takashigoto1207@gmail.com>
2022-05-01 22:03:55 +0900

    20220501_01

---
+++ abc2xx/abc211/abc211_c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  int n = S.size();
+  vector<vector<int>> dp(n + 1, vector<int>(9));
+  rep(i, n + 1) dp[i][0] = 1;
+  const int mod = 1000000007;
+  string t = "chokudai";
+  rep(i, n) rep(j, 8) {
+    if (S[i] != t[j])
+      dp[i + 1][j + 1] = dp[i][j + 1];
+    else
+      dp[i + 1][j + 1] = (dp[i][j + 1] + dp[i][j]) % mod;
+  }
+  cout << dp[n][8] << endl;
+  return 0;
+}
