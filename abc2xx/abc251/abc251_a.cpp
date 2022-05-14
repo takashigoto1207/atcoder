1000bc65bca4a42bde913f9d763ad50f3eaa60e5
takashigoto1207 <takashigoto1207@gmail.com>
2022-05-15 07:40:56 +0900

    20220515_01

---
+++ abc2xx/abc251/abc251_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  rep(i, 6 / S.size()) cout << S;
+  cout << endl;
+  return 0;
+}
