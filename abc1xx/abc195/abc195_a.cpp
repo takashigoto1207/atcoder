bb49362bedc7d1f3606bc417b34edc8538288974
takashigoto1207 <takashigoto1207@gmail.com>
2022-08-17 06:08:52 +0900

    20220817_01

---
+++ abc1xx/abc195/abc195_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int M, H;
+  cin >> M >> H;
+
+  if (H % M == 0)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
