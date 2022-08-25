9ad76c5055d440190a94a7def617f308d37a4dc0
takashigoto1207 <takashigoto1207@gmail.com>
2022-08-25 12:25:29 +0900

    20220825_01

---
+++ abc1xx/abc193/abc193_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  double A, B;
+  cin >> A >> B;
+
+  cout << fixed << setprecision(10) << 100 - B * 100 / A << endl;
+  return 0;
+}
