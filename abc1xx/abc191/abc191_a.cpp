c034ca56108b7b719cd16b210b6f43316d10a3de
takashigoto1207 <takashigoto1207@gmail.com>
2022-09-02 00:23:40 +0900

    20220902_01

---
+++ abc1xx/abc191/abc191_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int V, T, S, D;
+  cin >> V >> T >> S >> D;
+
+  if (V * T <= D && D <= V * S)
+    cout << "No" << endl;
+  else
+    cout << "Yes" << endl;
+  return 0;
+}
