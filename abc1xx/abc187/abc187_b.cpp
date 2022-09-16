3ad4a15c7ab478a822d5c7801efc72b533ef543f
takashigoto1207 <takashigoto1207@gmail.com>
2022-09-17 07:57:22 +0900

    20220917_01

---
+++ abc1xx/abc187/abc187_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> x(N), y(N);
+  rep(i, N) cin >> x[i] >> y[i];
+
+  int ans = 0;
+  rep(i, N) for (int j = i + 1; j < N;
+                 j++) if (abs(y[i] - y[j]) <= abs(x[i] - x[j])) ans++;
+
+  cout << ans << endl;
+  return 0;
+}
