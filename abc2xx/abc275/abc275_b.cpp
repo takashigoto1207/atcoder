b2004bd0de325316276ef63203afdfbaa5012ce0
takashigoto1207 <takashigoto1207@gmail.com>
2022-10-31 20:19:48 +0900

    20221031_01

---
+++ abc2xx/abc275/abc275_b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll mod = 998244353;
+
+ll f(ll x, ll y, ll z) { return (((x * y) % mod) * z) % mod; }
+int main() {
+  ll A, B, C, D, E, F;
+  cin >> A >> B >> C >> D >> E >> F;
+
+  cout << (f(A, B, C) - f(D, E, F) + mod) % mod << endl;
+  return 0;
+}
