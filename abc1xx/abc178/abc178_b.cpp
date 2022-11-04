e2c734467b0ad407838cf97cbb5cafb823bdcb2e
takashigoto1207 <takashigoto1207@gmail.com>
2022-11-05 08:34:13 +0900

    20221105_01

---
+++ abc1xx/abc178/abc178_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  ll a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  cout << max({a * c, a * d, b * c, b * d}) << endl;
+  return 0;
+}
