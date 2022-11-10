d4a89678b7b73070a202e0a78ffab81cf9a56072
takashigoto1207 <takashigoto1207@gmail.com>
2022-11-11 01:10:12 +0900

    20221111_01

---
+++ abc1xx/abc177/abc177_c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll mod = 1000000007;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  rep(i, N) cin >> A[i];
+
+  ll calc = 0;
+  ll ans = 0;
+  rep(i, N) {
+    ans = (ans + A[i] * calc) % mod;
+    calc = (calc + A[i]) % mod;
+  }
+  cout << ans << endl;
+  return 0;
+}
