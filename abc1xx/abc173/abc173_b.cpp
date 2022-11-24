8ac9aeffe7d3591fea9d757657224c6cfe76adb4
takashigoto1207 <takashigoto1207@gmail.com>
2022-11-24 19:06:44 +0900

    20221124_01

---
+++ abc1xx/abc173/abc173_b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N;
+
+  map<string, int> mp;
+
+  rep(i, N) cin >> S, mp[S]++;
+
+  cout << "AC x " << mp["AC"] << endl;
+  cout << "WA x " << mp["WA"] << endl;
+  cout << "TLE x " << mp["TLE"] << endl;
+  cout << "RE x " << mp["RE"] << endl;
+  return 0;
+}
