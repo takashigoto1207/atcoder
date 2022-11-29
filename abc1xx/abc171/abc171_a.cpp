2bf9d6ab0c4ef16210ba04ad1f60d95f33d38638
takashigoto1207 <takashigoto1207@gmail.com>
2022-11-29 22:19:25 +0900

    20221129_01

---
+++ abc1xx/abc171/abc171_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  char a;
+  cin >> a;
+
+  if (isupper(a))
+    cout << "A" << endl;
+  else
+    cout << "a" << endl;
+  return 0;
+}
