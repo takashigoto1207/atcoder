a21dd2656819098352c9964e74aa3401fa75e567
takashigoto1207 <takashigoto1207@gmail.com>
2022-12-08 01:02:38 +0900

    20221208_01

---
+++ abc1xx/abc170/abc170_b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int X, Y;
+  cin >> X >> Y;
+
+  if (Y % 2 == 0 && X * 2 <= Y && Y <= X * 4)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
