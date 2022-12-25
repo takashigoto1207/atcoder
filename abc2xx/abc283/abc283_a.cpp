b0baaa1f1e9d697a5432fa020b60f7dfaa14b869
takashigoto1207 <takashigoto1207@gmail.com>
2022-12-25 13:06:39 +0900

    20221225_01

---
+++ abc2xx/abc283/abc283_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int ans = 1;
+  rep(i, B) ans *= A;
+  cout << ans << endl;
+  return 0;
+}
