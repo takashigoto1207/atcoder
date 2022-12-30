ab149570009b63776addc03790c6cafe30ce5d91
takashigoto1207 <takashigoto1207@gmail.com>
2022-12-30 10:25:50 +0900

    20221230_01

---
+++ abc1xx/abc165/abc165_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  ll X;
+  cin >> X;
+
+  int ans = 0;
+  ll s = 100;
+  while (s < X) {
+    s += s / 100;
+    ans++;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
