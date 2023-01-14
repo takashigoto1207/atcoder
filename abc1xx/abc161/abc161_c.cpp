3650ba3b19d50cfc07b3b2cd92723e3406e8df03
takashigoto1207 <takashigoto1207@gmail.com>
2023-01-14 10:49:42 +0900

    20230114_01

---
+++ abc1xx/abc161/abc161_c.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  ll N, K;
+  cin >> N >> K;
+
+  cout << min(N % K, abs(N % K - K)) << endl;
+  return 0;
+}
