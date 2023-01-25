53fc610ffaf89e7f375e11c0dad4727c5b8f570c
takashigoto1207 <takashigoto1207@gmail.com>
2023-01-25 12:21:37 +0900

    20230125_01

---
+++ abc1xx/abc157/abc157_b.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+vector<vector<int>> A(3, vector<int>(3));
+  rep(i, 3) rep(j, 3) cin >> A[i][j];
+
+  int N;
+  cin >> N;
+
+  int b = 0;
+  rep(i, N) {
+    cin >> b;
+    rep(i, 3) {
+      rep(j, 3) {
+        if (A[i][j] == b) A[i][j] = 0;
+        if (
+            // 横
+            (A[0][0] == 0 && A[0][1] == 0 && A[0][2] == 0) ||
+            (A[1][0] == 0 && A[1][1] == 0 && A[1][2] == 0) ||
+            (A[2][0] == 0 && A[2][1] == 0 && A[2][2] == 0) ||
+            // 縦
+            (A[0][0] == 0 && A[1][0] == 0 && A[2][0] == 0) ||
+            (A[0][1] == 0 && A[1][1] == 0 && A[2][1] == 0) ||
+            (A[0][2] == 0 && A[1][2] == 0 && A[2][2] == 0) ||
+            // 斜め
+            (A[0][0] == 0 && A[1][1] == 0 && A[2][2] == 0) ||
+            (A[0][2] == 0 && A[1][1] == 0 && A[2][0] == 0)) {
+          cout << "Yes" << endl;
+          return 0;
+        }
+      }
+    }
+  }
+
+  cout << "No" << endl;
+  return 0;
+}
