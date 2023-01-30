2529a13497b28ab49dd65b4c21ee9479d9bdfe6f
takashigoto1207 <takashigoto1207@gmail.com>
2023-01-30 12:51:42 +0900

    20230130_01

---
+++ abc2xx/abc286/abc286_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+  cout << regex_replace(S, regex("na"), "nya") << endl;
+  return 0;
+}
