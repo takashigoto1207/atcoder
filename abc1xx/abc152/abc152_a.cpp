bb7eb8133889ae6d90f78bb793b9280a7bc285f3
takashigoto1207 <takashigoto1207@gmail.com>
2023-02-15 23:18:43 +0900

    20230215_01

---
+++ abc1xx/abc152/abc152_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  if (N == M)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
