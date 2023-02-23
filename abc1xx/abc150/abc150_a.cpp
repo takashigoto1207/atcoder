995bf254464c6ff32b658000b487822bf80fda64
takashigoto1207 <takashigoto1207@gmail.com>
2023-02-23 11:23:32 +0900

    20230223_01

---
+++ abc1xx/abc150/abc150_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int K, X;
+  cin >> K >> X;
+
+  if (X <= 500 * K)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
