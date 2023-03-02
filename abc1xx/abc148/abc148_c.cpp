3d825a0c4251bef567bc3e07722b3737f7261160
takashigoto1207 <takashigoto1207@gmail.com>
2023-03-03 00:46:51 +0900

    20230303_01

---
+++ abc1xx/abc148/abc148_c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+int main() {
+  ll A, B;
+  cin >> A >> B;
+
+  cout << A * B / gcd(A, B) << endl;
+  return 0;
+}
