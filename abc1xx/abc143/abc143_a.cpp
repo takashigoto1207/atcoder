63c91b0de08b2f154fa19dd06260ced6f4f67841
takashigoto1207 <takashigoto1207@gmail.com>
2023-03-16 20:54:21 +0900

    20230316_01

---
+++ abc1xx/abc143/abc143_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  cout << max(0, A - 2 * B) << endl;
+  return 0;
+}
