c3ba1b9631dc5c234c3d38eee3be099f82c08211
takashigoto1207 <takashigoto1207@gmail.com>
2023-03-18 11:54:36 +0900

    20230318_01

---
+++ abc1xx/abc143/abc143_c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  string T;
+  rep(i, N) {
+    if (i >= 1 && S[i - 1] != S[i]) T += S[i];
+  }
+  cout << T.size() + 1 << endl;
+  return 0;
+}
