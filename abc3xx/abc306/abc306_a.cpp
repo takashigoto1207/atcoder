a83a9346a076904589cb2e0494d5d8339ce15e7a
takashigoto1207 <takashigoto1207@gmail.com>
2023-06-21 20:22:09 +0900

    20230621_01

---
+++ abc3xx/abc306/abc306_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  rep(i, N) cout << S[i] << S[i];
+  cout << endl;
+  return 0;
+}
