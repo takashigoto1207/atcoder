633345049973617803adefce09aaffc6830153be
takashigoto1207 <takashigoto1207@gmail.com>
2023-06-29 06:58:38 +0900

    20230629_01

---
+++ abc1xx/abc110/abc110_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  cout << max({A, B, C}) * 9 + A + B + C << endl;
+  return 0;
+}
