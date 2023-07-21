0d9c06a287d150cbc5305f1959c9a1bbef22d992
takashigoto1207 <takashigoto1207@gmail.com>
2023-07-22 07:08:30 +0900

    20230722_01

---
+++ abc1xx/abc100/abc100_c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> a(N);
+  rep(i, N) cin >> a[i];
+
+  ll ans = 0;
+  rep(i, N) {
+    while (a[i] % 2 == 0) {
+      a[i] /= 2;
+      ans++;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
