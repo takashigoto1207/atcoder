c3b15ab049f4a93d3467046fe3788696e827260e
takashigoto1207 <takashigoto1207@gmail.com>
2023-07-23 17:48:59 +0900

    20230723_01

---
+++ abc3xx/abc311/abc311_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  cout << max({S.find("A"), S.find("B"), S.find("C")}) + 1 << endl;
+  return 0;
+}
