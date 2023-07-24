c81b379e62209c246ce2249b1fb307b3134d032e
takashigoto1207 <takashigoto1207@gmail.com>
2023-07-25 00:45:05 +0900

    20230725_01

---
+++ abc0xx/abc099/abc099_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  if (N <= 999)
+    cout << "ABC" << endl;
+  else
+    cout << "ABD" << endl;
+  return 0;
+}
