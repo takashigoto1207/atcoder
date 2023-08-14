369eb47efc8f6b0d716a8ae82259e5f1e1389821
takashigoto1207 <takashigoto1207@gmail.com>
2023-08-14 12:02:38 +0900

    20230814_01

---
+++ abc0xx/abc090/abc090_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string c1, c2, c3;
+  cin >> c1 >> c2 >> c3;
+
+  cout << c1[0] << c2[1] << c3[2] << endl;
+  return 0;
+}
