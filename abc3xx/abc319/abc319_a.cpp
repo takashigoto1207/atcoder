694f840c5b8b1dbd198eeca13f46741647af6b3a
takashigoto1207 <takashigoto1207@gmail.com>
2023-09-11 07:30:38 +0900

    20230911_01

---
+++ abc3xx/abc319/abc319_a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  map<string, int> mp;
+  mp["tourist"] = 3858;
+  mp["ksun48"] = 3679;
+  mp["Benq"] = 3658;
+  mp["Um_nik"] = 3648;
+  mp["apiad"] = 3638;
+  mp["Stonefeang"] = 3630;
+  mp["ecnerwala"] = 3613;
+  mp["mnbvmar"] = 3555;
+  mp["newbiedmy"] = 3516;
+  mp["semiexp"] = 3481;
+
+  string S;
+  cin >> S;
+
+  cout << mp[S] << endl;
+  return 0;
+}
