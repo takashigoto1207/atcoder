2e65085e8cc7451fe3820d64edc76fb9ad9b3987
takashigoto1207 <takashigoto1207@gmail.com>
2023-09-19 07:39:17 +0900

    20230919_01

---
+++ abc0xx/abc078/abc078_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int X, Y, Z;
+  cin >> X >> Y >> Z;
+
+  cout << (X - Z) / (Y + Z) << endl;
+  return 0;
+}
