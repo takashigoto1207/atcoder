f28370fefea38bf06ea7228ea2b32e3b0afe6daf
takashigoto1207 <takashigoto1207@gmail.com>
2023-11-08 12:14:47 +0900

    20231108_01

---
+++ abc0xx/abc058/abc058_b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string O, E;
+  cin >> O >> E;
+
+  rep(i, E.size()) cout << O[i] << E[i];
+  if (O.size() - E.size() == 1) cout << O[O.size() - 1];
+  cout << endl;
+  return 0;
+}
