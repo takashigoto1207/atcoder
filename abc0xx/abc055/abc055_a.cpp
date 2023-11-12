d839844ecf0c508c29ca775ab8b8cac98983fe1d
takashigoto1207 <takashigoto1207@gmail.com>
2023-11-13 06:41:43 +0900

    20231113_01

---
+++ abc0xx/abc055/abc055_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << 800 * N - 200 * (N / 15) << endl;
+  return 0;
+}
