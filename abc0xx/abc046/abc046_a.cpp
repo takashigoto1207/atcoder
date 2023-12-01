c55d2125f80868b39f80e5f330f0c9c4f8d3c821
takashigoto1207 <takashigoto1207@gmail.com>
2023-12-01 21:49:19 +0900

    20231201_01

---
+++ abc0xx/abc046/abc046_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  set<int> st;
+  st.insert(a);
+  st.insert(b);
+  st.insert(c);
+  cout << st.size() << endl;
+  return 0;
+}
