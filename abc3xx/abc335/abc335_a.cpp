5ff5bb9094f75ff152857d1654e1978d8d1890e7
takashigoto1207 <takashigoto1207@gmail.com>
2024-01-07 09:42:12 +0900

    20240107_01

---
+++ abc3xx/abc335/abc335_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  S.pop_back();
+  S.push_back('4');
+  cout << S << endl;
+  return 0;
+}
