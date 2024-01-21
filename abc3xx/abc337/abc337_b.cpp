2c03ed2023be1ac5b3345e3b76b7c3c76068c8bb
takashigoto1207 <takashigoto1207@gmail.com>
2024-01-22 00:12:00 +0900

    20240122_01

---
+++ abc3xx/abc337/abc337_b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S, T;
+  cin >> S;
+
+  T = S;
+  sort(T.begin(), T.end());
+
+  if (T == S)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
