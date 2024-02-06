9d6bae03e93d16f2ede20e6a42f01b9bee4b6fbe
takashigoto1207 <takashigoto1207@gmail.com>
2024-02-07 08:36:36 +0900

    20240207_01

---
+++ abc0xx/abc020/abc020_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int Q;
+  cin >> Q;
+
+  if (Q == 1)
+    cout << "ABC" << endl;
+  else
+    cout << "chokudai" << endl;
+  return 0;
+}
