5a4950dba596360de98406f959f7a8a602c30726
takashigoto1207 <takashigoto1207@gmail.com>
2024-02-16 12:11:33 +0900

    20240216_01

---
+++ abc0xx/abc015/abc015_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string A, B;
+  cin >> A >> B;
+
+  if (A.size() > B.size())
+    cout << A << endl;
+  else
+    cout << B << endl;
+  return 0;
+}
