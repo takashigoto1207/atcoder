5572f74369fa541e1d572b78c85cd5eb3971cf9e
takashigoto1207 <takashigoto1207@gmail.com>
2024-03-05 07:48:45 +0900

    20240305_01

---
+++ abc3xx/abc343/abc343_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  cout << (A + B + 1) % 10 << endl;
+  return 0;
+}
