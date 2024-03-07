cc53b12193de4198f752bcf24f63429df6ed5cc1
takashigoto1207 <takashigoto1207@gmail.com>
2024-03-08 07:46:14 +0900

    20240308_01

---
+++ abc0xx/abc007/abc007_b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string A;
+  cin >> A;
+  if (A == "a")
+    cout << -1 << endl;
+  else
+    cout << "a" << endl;
+  return 0;
+}
