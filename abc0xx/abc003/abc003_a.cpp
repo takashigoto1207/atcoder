39a3f7281cbe35d24cd151f30c2122946403a183
takashigoto1207 <takashigoto1207@gmail.com>
2024-03-15 08:11:11 +0900

    20240315_01

---
+++ abc0xx/abc003/abc003_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << (N + 1) * 10000 / 2 << endl;
+  return 0;
+}
