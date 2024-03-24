e7fc7e7cb6e8f221a7ea4dcec30fecdf2751da4a
takashigoto1207 <takashigoto1207@gmail.com>
2024-03-24 22:34:42 +0900

    20240324_01

---
+++ abc3xx/abc346/abc346_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  rep(i, N) cin >> A[i];
+  rep(i, N - 1) cout << A[i] * A[i + 1] << " ";
+  cout << endl;
+  return 0;
+}
