1c58d81efceaad4680dca4a9e1780fcb98b0e660
takashigoto1207 <takashigoto1207@gmail.com>
2024-05-30 08:13:07 +0900

    20240530_01

---
+++ abc3xx/abc355/abc355_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  if (A == B)
+    cout << -1 << endl;
+  else
+    cout << 6 - A - B << endl;
+  return 0;
+}
