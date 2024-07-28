de2220ede9e59a2fab492a09d5862ead22ff540a
takashigoto1207 <takashigoto1207@gmail.com>
2024-07-29 08:25:16 +0900

    20240729_01

---
+++ abc3xx/abc363/abc363_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int R;
+  cin >> R;
+  cout << 100 - R % 100 << endl;
+  return 0;
+}
