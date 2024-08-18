3f4467dd8fd843e7f44938830d1e515b7b7a4799
takashigoto1207 <takashigoto1207@gmail.com>
2024-08-19 00:13:46 +0800

    20240819_01

---
+++ abc3xx/abc367/abc367_a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  if (B < C) {
+    if (B < A && A < C)
+      cout << "No" << endl;
+    else
+      cout << "Yes" << endl;
+  } else {
+    if (C < A && A < B)
+      cout << "Yes" << endl;
+    else
+      cout << "No" << endl;
+  }
+  return 0;
+}
