5ac1eb7f88426b17d8902857cd050daae7e65db4
takashigoto1207 <takashigoto1207@gmail.com>
2024-08-20 21:36:57 +0800

    20240820_01

---
+++ abc3xx/abc367/abc367_b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  double x;
+  cin >> x;
+  cout << x << endl;
+  return 0;
+}
