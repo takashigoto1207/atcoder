6c86b43533ee049b7a0f08d001e83bab71d8fb5c
takashigoto1207 <takashigoto1207@gmail.com>
2024-08-31 18:01:46 +0900

    20240831_01

---
+++ arc0xx/arc006/arc006_a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+map<int, int> E;
+  map<int, int> L;
+  int B, n;
+  rep(i, 6) {
+    cin >> n;
+    E[n]++;
+  }
+  cin >> B;
+  rep(i, 6) {
+    cin >> n;
+    L[n]++;
+  }
+
+  int calc = 0;
+  rep(i, 10) calc += E[i] * L[i];
+
+  if (calc == 6)
+    cout << 1 << endl;
+  else if (calc == 5 && L[B] == 1)
+    cout << 2 << endl;
+  else if (calc == 5)
+    cout << 3 << endl;
+  else if (calc == 4)
+    cout << 4 << endl;
+  else if (calc == 3)
+    cout << 5 << endl;
+  else
+    cout << 0 << endl;
+  return 0;
+}
