9f7ddee6d36ce23e9c8efe91bff0aab44ab8979e
takashigoto1207 <takashigoto1207@gmail.com>
2024-09-02 08:15:17 +0900

    20240902_01

---
+++ arc0xx/arc007/arc007_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+    char X;
+  string s;
+  cin >> X >> s;
+
+  rep(i, s.size()) {
+    if (s[i] == X) continue;
+    cout << s[i];
+  }
+  cout << endl;
+  return 0;
+}
