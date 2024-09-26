7615cdece0d030ed892e4878e3150558cfd88de3
takashigoto1207 <takashigoto1207@gmail.com>
2024-09-26 20:10:28 +0900

    20240926_01

---
+++ arc0xx/arc014/arc014_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  if (N % 2 == 0)
+    cout << "Blue" << endl;
+  else
+    cout << "Red" << endl;
+  return 0;
+}
