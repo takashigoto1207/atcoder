5ca93a841b1f2ffaa22fc64ef0c5ca1d4b41a861
takashigoto1207 <takashigoto1207@gmail.com>
2024-09-28 08:37:47 +0900

    20240928_01

---
+++ arc0xx/arc015/arc015_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  double n;
+  cin >> n;
+
+  cout << setprecision(10) << (9.0 / 5.0 * n) + 32 << endl;
+  return 0;
+}
