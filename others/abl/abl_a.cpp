3d2e174567a7376c1b7f84d430eabe09468e6fe7
takashigoto1207 <takashigoto1207@gmail.com>
2024-11-23 10:20:43 +0900

    20241123_01

---
+++ others/abl/abl_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int K;
+  cin >> K;
+
+  rep(i, K) cout << "ACL";
+  cout << endl;
+  return 0;
+}
