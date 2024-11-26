414552b19f7192880923645fd1c859d1cd20c031
takashigoto1207 <takashigoto1207@gmail.com>
2024-11-26 22:07:25 +0900

    20241126_01

---
+++ others/tokiomarine2020/tokiomarine2020_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  cout << S.substr(0, 3) << endl;
+  return 0;
+}
