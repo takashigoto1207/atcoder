cdec5f810f2728cb28db7650d02f794b30fb672c
takashigoto1207 <takashigoto1207@gmail.com>
2024-12-13 08:11:28 +0900

    20241213_01

---
+++ others/sumitb2019/sumitb2019_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int M1, D1, M2, D2;
+  cin >> M1 >> D1 >> M2 >> D2;
+
+  if (D2 == 1)
+    cout << 1 << endl;
+  else
+    cout << 0 << endl;
+  return 0;
+}
