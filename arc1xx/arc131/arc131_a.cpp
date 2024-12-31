2d27c3efc6db58b58c9e0fad93ad7e2a835f8240
takashigoto1207 <takashigoto1207@gmail.com>
2024-12-31 16:43:33 +0900

    20241231_01

---
+++ arc1xx/arc131/arc131_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  ll A, B;
+  cin >> A >> B;
+  cout << 1000000000 * A + 5 * B << endl;
+  return 0;
+}
