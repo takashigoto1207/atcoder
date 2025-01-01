47dafdeed74ee2d0103e031c4a5443483ac5723f
takashigoto1207 <takashigoto1207@gmail.com>
2025-01-02 07:29:58 +0900

    20250102_01

---
+++ others/tenka1_2018/tenka1_2018_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S.size() == 2)
+    cout << S << endl;
+  else {
+    reverse(S.begin(), S.end());
+    cout << S << endl;
+  }
+  return 0;
+}
