f791c6ad04b0410c24be2865f75ac4f9f76d121b
takashigoto1207 <takashigoto1207@gmail.com>
2025-01-20 12:43:25 +0900

    20250120_01

---
+++ abc3xx/abc389/abc389_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  cout << (int)(S[0] - '0') * (int)(S[2] - '0') << endl;
+  return 0;
+}
