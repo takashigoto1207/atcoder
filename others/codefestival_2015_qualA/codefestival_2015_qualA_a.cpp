651dce8c2cf928da749a61b3051f79d2eab5c51f
takashigoto1207 <takashigoto1207@gmail.com>
2025-01-28 08:12:28 +0900

    20250128_01

---
+++ others/codefestival_2015_qualA/codefestival_2015_qualA_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+  S[S.size() - 1] = '5';
+  cout << S << endl;
+  return 0;
+}
