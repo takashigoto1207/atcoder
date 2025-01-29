87b6a0f663f3f195a953d92a2ccbf6832f15b19b
takashigoto1207 <takashigoto1207@gmail.com>
2025-01-30 08:46:27 +0900

    20250130_01

---
+++ others/code_festival_2017_qualc/code_festival_2017_qualc_a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  rep(i, S.size() - 1) {
+    if (S[i] == 'A' && S[i + 1] == 'C') {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
