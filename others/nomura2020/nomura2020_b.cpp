96c2464e938657273b418091d8cf906063600158
takashigoto1207 <takashigoto1207@gmail.com>
2025-02-15 00:48:11 +0900

    20250215_01

---
+++ others/nomura2020/nomura2020_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  replace(S.begin(), S.end(), '?', 'D');
+  cout << S << endl;
+  return 0;
+}
