57ab4b326294dd232710e4bea738e1895f5364b9
takashigoto1207 <takashigoto1207@gmail.com>
2025-02-23 22:18:40 +0900

    20250223_01

---
+++ abc3xx/abc394/abc394_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  for (char x : S)
+    if (x == '2') cout << x;
+  cout << endl;
+  return 0;
+}
