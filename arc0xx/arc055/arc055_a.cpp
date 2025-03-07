039e79395be4424f3cc346c4bb50f5945bb4217d
takashigoto1207 <takashigoto1207@gmail.com>
2025-03-07 09:33:12 +0900

    20250307_01

---
+++ arc0xx/arc055/arc055_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << 1;
+  rep(i, N - 1) cout << 0;
+  cout << 7 << endl;
+  return 0;
+}
