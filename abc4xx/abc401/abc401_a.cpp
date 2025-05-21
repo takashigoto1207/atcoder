f1cd1b58f61c7df8aaaf730996a78999a00c1aaa
takashigoto1207 <takashigoto1207@gmail.com>
2025-05-22 08:25:14 +0900

    20250522_01

---
+++ abc4xx/abc401/abc401_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int S;
+  cin >> S;
+
+  if (200 <= S && S <= 299)
+    cout << "Success" << endl;
+  else
+    cout << "Failure" << endl;
+  return 0;
+}
