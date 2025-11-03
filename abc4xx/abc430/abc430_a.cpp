c9859395c867ac6335af2cf7b58e8d7a4dbe5000
takashigoto1207 <takashigoto1207@gmail.com>
2025-11-03 22:17:22 +0900

    20251103_01

---
+++ abc4xx/abc430/abc430_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int A, B, C, D;
+  cin >> A >> B >> C >> D;
+
+  if (C >= A && D < B) {
+    cout << "Yes" << endl;
+    return 0;
+  }
+  cout << "No" << endl;
+  return 0;
+}
