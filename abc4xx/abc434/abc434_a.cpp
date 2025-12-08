db7398407e51213ef5bb00bdf71a2df317aeab04
Takashi Goto <takashigoto1207@gmail.com>
2025-12-08 20:19:02 +0900

    20251208_01

---
+++ abc4xx/abc434/abc434_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int W, B;
+  cin >> W >> B;
+
+  cout << W * 1000 / B + 1 << endl;
+  return 0;
+}
