20f4316534b8c84da5164773bf72b936189bbf61
Takashi Goto <takashigoto1207@gmail.com>
2026-08-09 22:18:11 +0900

    20260809_01

---
+++ abc4xx/abc470/abc470_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  rep(i, N) {
+    if ((i + 1) % 3 == 0)
+      cout << "Fizz" << endl;
+    else
+      cout << i + 1 << endl;
+  }
+  return 0;
+}
