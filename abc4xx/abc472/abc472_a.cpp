f7bac548974898efb857cf7f6fcc5df50385fd75
Takashi Goto <takashigoto1207@gmail.com>
2026-08-23 10:36:41 +0900

    20260823_01

---
+++ abc4xx/abc472/abc472_a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  for (char c : S) {
+    if (c == 'A')
+      cout << c;
+    else
+      cout << ".";
+  }
+  cout << endl;
+  return 0;
+}
