82a77d031a8199226fb055015954d15a8a7c7add
Takashi Goto <takashigoto1207@gmail.com>
2026-08-30 00:24:02 +0900

    20260830_02

---
+++ abc4xx/abc473/abc473_b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N, A;
+  cin >> N;
+
+  map<int, int> mp;
+  rep(i, N) {
+    cin >> A;
+    mp[A]++;
+  }
+
+  int ans = 0;
+  for (auto x : mp)
+    if (x.second % 2 == 1) ans += x.first;
+  cout << ans << endl;
+  return 0;
+}
