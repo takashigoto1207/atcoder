e968b2ac8ba02b566aefab0284286b1795e2a4db
Takashi Goto <takashigoto1207@gmail.com>
2026-08-30 23:42:06 +0900

    20260830_04

--- abc2xx/abc299/abc299_b.cpp
+++ abc2xx/abc299/abc299_b.cpp
@@ -7,13 +7,21 @@ using namespace atcoder;
 using ll = long long;
 
 int main() {
-  int N;
-  string S;
-  cin >> N >> S;
+  int N, T;
+  cin >> N >> T;
 
-  if (S.find('|') < S.find('*') && S.find('*') < S.rfind('|'))
-    cout << "in" << endl;
-  else
-    cout << "out" << endl;
+  vector<int> C(N), R(N);
+  rep(i, N) cin >> C[i];
+  rep(i, N) cin >> R[i];
+
+  int mx = 0, ans = 0;
+  rep(i, N) if (C[i] == T && R[i] > mx) ans = i + 1, mx = R[i];
+  if (ans != 0) {
+    cout << ans << endl;
+    return 0;
+  }
+
+  rep(i, N) if (C[i] == C[0] && R[i] > mx) ans = i + 1, mx = R[i];
+  cout << ans << endl;
   return 0;
 }
