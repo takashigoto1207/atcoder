853fc0395dc34a518d6a19eabff87789fb3883ff
Takashi Goto <takashigoto1207@gmail.com>
2026-09-13 00:20:37 +0900

    20260913_01

---
+++ abc4xx/abc475/abc475_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  rep(i, S.size() - 1) cout << S[i] << "o";
+  cout << S[S.size() - 1] << endl;
+  return 0;
+}
